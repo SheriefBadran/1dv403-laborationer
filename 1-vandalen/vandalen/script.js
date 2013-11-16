aac6f8523f79c5838c37a41f918e613597643f8a
SheriefBadran <sheriefbadran@Sheriefs-MacBook-Air.local>
2013-11-16 15:24:53 +0100

    Uppgift vandalen: Har förberett med en lista över vad som skall göras.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,24 @@
 "use strict";
 
 var makePerson = function(persArr){
-
-
 	// Din kod här...
 
-}
+	// Calculate minAge
 
+	// Calculate maxAge
+
+	// Calculate averageAge
+
+	// Create string containing all the persons names
+
+	// Create an object containing all data
+
+	// return the object 
+
+};
+
+var persons = [{ name: "John Häggerud", age: 36 }, { name: "Johan Leitet", age: 37 }, { name: "Mats Loock", age: 47 }];
+
+var result = makePerson(persons);
+
+console.log(result);
