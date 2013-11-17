079c99e848e1279028aecc2bbb2f64a1f1ccadae
SheriefBadran <sheriefbadran@Sheriefs-MacBook-Air.local>
2013-11-17 10:14:56 +0100

    Har använt Array objektets prototype egenskaper för ES 5 för att ta beräkna och ta ut de värden som skall returneras i ett objekt. Hittills 3 av 7 assertions uppfyllda i testet.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,60 @@
 
 var makePerson = function(persArr){
 	// Din kod här...
+	var persons = [];
+	var personAges;
+	var personNames;
+	var personObj = {};
+	//var minAge;
+	var ageSum;
 
-	// Calculate minAge
+	console.log(persArr);
+
+	// Filter out all person arrays that don't have the born property.
+	persons = persArr.filter(function(person, i){
+		return !person.hasOwnProperty('born');
+	});
+
+	// Retrieve age values from all objects in the array.
+	personAges = persons.map(function(person, i){
+		return person.age;
+	});
+
+	// Retrieve name values from all objects in the array.
+	personNames = persons.map(function(person, i){
+		return person.name;
+	});
+
+	// Sum all ages.
+	ageSum = personAges.reduce(function(prevAge, age, i, personAges){
+		return prevAge + age;
+	});
+
+	// Calculate average age and initialize it to personObj.
+	personObj.averageAge = Math.round(ageSum/personAges.length);
+
+	// Get max age and initialize it to personObj.
+	personObj.maxAge = personAges.reduce(function(prevAge, age, i, personAges){
+		return Math.max(prevAge, age);
+	});
+
+	// Get min age and initialize it to personObj.
+	personObj.minAge = personAges.reduce(function(prevAge, age, i, personAges){
+		return Math.min(prevAge, age);
+	});
+
+	// Assamble all names to one string and initialize it to personObj
+	personObj.names = personNames.reduce(function(prevName, name, i, personNames){
+		return prevName + ", " + name;
+	});
+
+	console.log(persons);
+	console.log(personObj.averageAge);
+	console.log(personAges);
+	console.log(personNames);
+	console.log(personObj);
+
+	return personObj;
 
 	// Calculate maxAge
 
@@ -13,12 +65,9 @@ var makePerson = function(persArr){
 
 	// Create an object containing all data
 
-	// return the object 
-
+	// return the object
 };
 
-var persons = [{ name: "John Häggerud", age: 36 }, { name: "Johan Leitet", age: 37 }, { name: "Mats Loock", age: 47 }];
+//var persons = [{ name: "John Häggerud", age: 36 }, { name: "Johan Leitet", age: 37 }, { name: "Mats Loock", age: 47 }];
 
-var result = makePerson(persons);
-
-console.log(result);
+//var result = makePerson(persons);
